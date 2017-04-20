944d96bc5dfb41176965882acd628337828d9295
Ben Drews <benjamindrews@gmail.com>
2017-04-20 19:47:26 -0400

    WIP

--- qasys.py
+++ qasys.py
@@ -14,8 +14,6 @@ class Question:
         self.tokens = tokens
         self.weights = weights
 
-#Usage
-
 TRAIN_Q_PATH = "hw5data/qadata/train/questions.txt"
 TEST_Q_PATH = "hw5data/qadata/test/questions.txt"
 STOP_WORDS_PATH = "hw5data/stopwords.txt"
@@ -23,7 +21,9 @@ TRAIN_TOPDOCS_PATH = "hw5data/topdocs/train/top_docs."
 TEST_TOPDOCS_PATH = "hw5data/topdocs/test/top_docs."
 
 SENT_DETECTOR = nltk.data.load('tokenizers/punkt/english.pickle')
-NER_TAGGER = StanfordNERTagger('/home/cs-students/18bfd2/bin/stanford-ner-2014-08-27/classifiers/english.all.3class.distsim.crf.ser.gz', '/home/cs-students/18bfd2/bin/stanford-ner-2014-08-27/stanford-ner.jar')
+NER_TAGGER = StanfordNERTagger('/home/cs-students/18bfd2/bin/stanford-ner-2014-08-27/classifiers/english.muc.7class.distsim.crf.ser.gz', '/home/cs-students/18bfd2/bin/stanford-ner-2014-08-27/stanford-ner.jar')
+
+predictionFile = open('prediction.txt', 'w')
 
 #Read in and process the questions located at path
 def getQuestions(path):
@@ -72,14 +72,14 @@ def getStopWords():
 def getTrainTopDocs(qNum):
     with codecs.open(TRAIN_TOPDOCS_PATH + str(qNum), 'r', 'cp437') as topdocsData:
         sentences =  SENT_DETECTOR.tokenize(topdocsData.read().strip())
-    return [[y.lower() for y in x.split()] for x in sentences]
+    return [x.split() for x in sentences]
 
 
 #Retrive a list of the tokens in the topdocs for associated question
 def getTestTopDocs(qNum):
     with codecs.open(TEST_TOPDOCS_PATH + str(qNum), 'r', 'cp437') as topdocsData:
         sentences =  SENT_DETECTOR.tokenize(topdocsData.read().strip())
-    return [[y.lower() for y in x.split()] for x in sentences]
+    return [x.split() for x in sentences]
 
 #Comptues magnitude of a vector. Helper for Cosine Similarity
 def mag(v):
@@ -100,40 +100,93 @@ def cosSim(vec1, vec2):
     else:
         return (numerator / denominator)
 
+#Find average distance to question keywords
+def keywordDistance(docSentence, question, ansIndex):
+    keywordIndices = []
+    for i in range(0, len(docSentence)):
+        if docSentence[i].lower() in question.tokens:
+            keywordIndices.append(i)
+
+    invResult = 0.0
+    for i in range(0, len(keywordIndices)):
+        invResult += math.sqrt(math.abs(ansIndex - i))
+    
+    return 1.0/invResult
+
+#For a given document sentence and set of query keywords, return a vector
+# that represents the frequency with which those keywords occury in the
+# sentence
 def getQueryVector(docSentence, query):
     keywordVector = [0 for x in range(0, len(query))]
     for token in docSentence:
-        if token in query:
-            keywordVector[query.index(token)] += 1
+        if token.lower() in query:
+            keywordVector[query.index(token.lower())] += 1
     return keywordVector
 
+#Sort the top docs for a given question by their cosine similarity to 
+# the keywords of the question.
 def sortTopDocs(sentences, question):
     sentences.sort(key=lambda x: cosSim(question.weights, getQueryVector(x, question.tokens)), reverse=True)
 
+#Method to edit most
+#Takes the top 5 documents and ranks which entities might be answers
 def extractAnswers(sortedDocs, question):
-
     possibleAnswers = []
     weights = []
-    for i in range(0, 10):
-        namedEntities = NER_TAGGER.tag(sortedDocs[i])
-        for j in range(0, len(namedEntities)):
-            entity = namedEntities[j]
+    nounTags = ['NN', 'NNP', 'NNS', 'NNPS']
 
-            if not (entity[1] == 'O' or entity[0] in question.tokens):
-                if not (entity[0] in possibleAnswers):
-                    possibleAnswers.append(entity[0])
-                    weights.append(1)
-                else:
-                    weights[possibleAnswers.index(entity[0])] += 1
-    return sorted(possibleAnswers, key=lambda x: weights[possibleAnswers.index(x)])
+    #Take top 5 topdocs
+
+    if question.qType is 'where' or question.qType is 'who':
+        for i in range(0, 5):
+            namedEntities = NER_TAGGER.tag(sortedDocs[i])
+            print str(namedEntities)
             
+            #Consider each entity tagged
+            for j in range(0, len(namedEntities)):
+                entity = namedEntities[j]
 
+                #If the entity is not a named entity or is part of the question, don't consider it
+                if not (entity[1] == 'O' or entity[0] in question.tokens):
+
+                #If the entity is not yet a possible answer add it, else increment it's weight
+                    if not (entity[0] in possibleAnswers):
+                        possibleAnswers.append(entity[0])
+                        weights.append(1)
+                    else:
+                        weights[possibleAnswers.index(entity[0])] += 1
+    elif question.qType is 'when' or 'how':
+        for i in range(0, 5):
+            partsOfSpeech = nltk.pos_tag(sortedDocs[i])
+            print str(partsOfSpeech)
+
+            #Consider each entity tagged
+            for j in range(0, len(partsOfSpeech)):
+                entity = namedEntities[j]
+                
+                #If the entity is not a named entity or is part of the question, don't consider it
+                if  entity[1] is 'CD' and not entity[0] in question.tokens:
+
+                #If the entity is not yet a possible answer add it, else increment it's weight
+                    if not (entity[0] in possibleAnswers):
+                        possibleAnswers.append(entity[0])
+                        weights.append(1)
+                    else:
+                        weights[possibleAnswers.index(entity[0])] += 1
+    elif question.qType is 'what' or 'which' or 'why' or 'unknown':
+    return sorted(possibleAnswers, key=lambda x: weights[possibleAnswers.index(x)])
+
+def writeAnswersToFile(possibleAnswers, qid):
+    predictionFile.write('qid ' + str(qid) + '\n')
+    for i in range(0, min(len(possibleAnswers), 10)):
+        predictionFile.write(possibleAnswers[i] + '\n')
+            
 if __name__ == "__main__":
     #Read in data
     trainingQs = getQuestions(TRAIN_Q_PATH)
     testQs = getQuestions(TEST_Q_PATH)
 
-    #Verify questions are being read correctly
+    #Attempt to answer training questions
     for q in trainingQs:
         print str(q.number) + "================================="
         print str(q.qType)
@@ -144,11 +197,9 @@ if __name__ == "__main__":
         sortTopDocs(topDocs, q)
         for i in range(0, 5):
             print "\nTopDoc number " + str(i + 1) + ": " + str(topDocs[i])
-            
-        possibleAnswers = extractAnswers(topDocs, q)
-        for i in range(0, min(len(possibleAnswers), 5)):
-            print "\nPossible answer: " + possibleAnswers[i]
 
-    
+        possibleAnswers = extractAnswers(topDocs, q)
+        writeAnswersToFile(possibleAnswers, q.number)
+    predictionFile.close()
     
 
